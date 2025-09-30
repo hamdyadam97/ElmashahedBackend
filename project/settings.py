73ed5294ad6365ac0051ed70d2fb52db8fd87d13
Hamdy <hamdyadam97@gmail.com>
2025-09-30 16:31:47 +0300

    push

--- project/settings.py
+++ project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -137,27 +138,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# REST_FRAMEWORK = {
-#     'DEFAULT_RENDERER_CLASSES': [
-#         'project.renderers.CustomJSONRenderer',
-#         'rest_framework.renderers.JSONRenderer',
-#         'rest_framework.renderers.BrowsableAPIRenderer',
-#     ],
-#
-#     'DEFAULT_FILTER_BACKENDS': [
-#         'django_filters.rest_framework.DjangoFilterBackend',
-#         'rest_framework.filters.SearchFilter',
-#         'rest_framework.filters.OrderingFilter',
-#     ],
-#
-#     # نخليها AllowAny هنا عشان ما تعطلش أي View،
-#
-#     'DEFAULT_AUTHENTICATION_CLASSES': [],
-#
-#     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 10
-# }
+
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
