e9c5e87d9f570340817ecaa0c1d2032af474363b
hamdyadam97 <hamdyadam97@gmail.com>
2026-07-27 18:12:02 +0300

    شةشسيي

--- permissions/views.py
+++ permissions/views.py
@@ -210,11 +210,12 @@ def generate_permission_pdf(permission):
                     .bg-watermark {{
                         position: fixed;
                         top: 0; left: 0; right: 0; bottom: 0;
+                        width: 100%;
+                        height: 100%;
                         background-image: url('data:image/png;base64,{institute_bg_b64}');
-                        background-size: cover;
+                        background-size: 100% 100%;
                         background-position: center;
                         background-repeat: no-repeat;
-                        opacity: 0.15;
                         z-index: -1;
                     }}
             """
